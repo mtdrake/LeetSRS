f5c4fd58d6036252a8e455e0a8fcbe91dd0f2bcc
Matt Drake <mattdrakeworks@gmail.com>
2025-08-03 16:09:05 -0700

    use tailwind

--- entrypoints/popup/views/debug/DebugView.tsx
+++ entrypoints/popup/views/debug/DebugView.tsx
@@ -2,8 +2,8 @@ import { DebugPanel } from '../../debug/DebugPanel';
 
 export function DebugView() {
   return (
-    <div className="debug-view">
-      <h2>Debug Information</h2>
+    <div className="p-4">
+      <h2 className="text-xl font-bold mb-4 text-primary">Debug Information</h2>
       <DebugPanel />
     </div>
   );

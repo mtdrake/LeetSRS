d5f5f5414ba2ba9e4eea42c3d0cf356e2a426b8f
Matt Drake <mattdrakeworks@gmail.com>
2025-08-02 19:44:41 -0700

    simplify background callback

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -4,34 +4,17 @@ import { MessageType, type MessageRequest } from '@/services/messages';
 
 export default defineBackground(() => {
   // Message handler for popup communication
-  browser.runtime.onMessage.addListener((request: MessageRequest, sender, sendResponse) => {
-    // Handle async operations
-    (async () => {
-      try {
-        switch (request.type) {
-          case MessageType.ADD_CARD: {
-            const card = await addCard(request.slug, request.name);
-            sendResponse(card);
-            break;
-          }
-          case MessageType.GET_ALL_CARDS: {
-            const cards = await getAllCards();
-            sendResponse(cards);
-            break;
-          }
-          default: {
-            // This should never happen with proper typing - exhaustive check
-            const _: never = request;
-            throw new Error('Unknown message type');
-          }
-        }
-      } catch (error) {
-        console.error('Background script error:', error);
-        sendResponse(null);
+  browser.runtime.onMessage.addListener(async (request: MessageRequest) => {
+    switch (request.type) {
+      case MessageType.ADD_CARD:
+        return await addCard(request.slug, request.name);
+      case MessageType.GET_ALL_CARDS:
+        return await getAllCards();
+      default: {
+        // This should never happen with proper typing - exhaustive check
+        const _: never = request;
+        throw new Error('Unknown message type');
       }
-    })();
-
-    // Return true to indicate we'll send a response asynchronously
-    return true;
+    }
   });
 });

02a05640f9dc616780b48424b8206df504224e14
Matt Drake <mattdrakeworks@gmail.com>
2025-08-02 22:55:08 -0700

    use slug as id

--- services/storage-keys.ts
+++ services/storage-keys.ts
@@ -1,6 +1,5 @@
 export const STORAGE_KEYS = {
   cards: 'local:leetreps:cards',
-  slugToCardId: 'local:leetreps:slugToCardId',
 } as const;
 
 export type StorageKey = (typeof STORAGE_KEYS)[keyof typeof STORAGE_KEYS];

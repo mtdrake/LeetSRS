7990ede71f61c33e6e7a5fa908fb44a97a5aab7e
Matt Drake <mattdrakeworks@gmail.com>
2025-08-03 16:06:20 -0700

    refactor views

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -9,13 +9,17 @@ import { DebugView } from './views/debug/DebugView';
 function App() {
   const [activeView, setActiveView] = useState<ViewId>('home');
 
+  const views: Record<ViewId, React.ReactNode> = {
+    home: <HomeView />,
+    stats: <StatsView />,
+    settings: <SettingsView />,
+    debug: <DebugView />,
+  };
+
   return (
     <div className="flex flex-col h-full relative bg-primary text-primary">
       <div className="flex-1 overflow-y-auto pb-[60px]">
-        {activeView === 'home' && <HomeView />}
-        {activeView === 'stats' && <StatsView />}
-        {activeView === 'settings' && <SettingsView />}
-        {activeView === 'debug' && <DebugView />}
+        {views[activeView]}
       </div>
 
       <BottomNav activeView={activeView} onNavigate={setActiveView} />
